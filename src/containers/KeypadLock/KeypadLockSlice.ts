e8b8f06dcce86e6bb2a729b3616015f0e9e72fdf
Meerim Maratova <maratovameerim@gmail.com>
2024-11-07 10:41:24 +0600

    created KeypadLockSlice with all the reducers

---
+++ src/containers/KeypadLock/KeypadLockSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Simulate } from 'react-dom/test-utils';
+import input = Simulate.input;
+
+interface keypadLockState {
+  input: string,
+  access: "pass"| "wrong" | null,
+  pin: string,
+}
+
+const initialState:keypadLockState = {
+  input: "",
+  access: null,
+  pin: "1337"
+}
+
+const keypadLockSlice = createSlice({
+  name: 'keypadLock',
+  initialState,
+  reducers: {
+    writeInput: (state, action) => {
+      if (state.input.length < 4) {
+        state.input += action.payload;
+      }
+    },
+    deleteInput: (state, action) => {
+      state.input = state.input.slice(0, -1);
+    },
+    checkPin: (state, action) => {
+      state.access = state.input === state.pin ? "pass" : "wrong";
+    }
+  }
+})
+
+export const {writeInput, deleteInput, checkPin} = keypadLockSlice.actions;
+export default keypadLockSlice.reducer;
